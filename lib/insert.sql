40153fada9aa258e532120179d97de608798ec64
Robert Sainton <robertsainton@outlook.com>
2022-12-01 17:57:44 -0500

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 3, 'M', 'brown', 'friendly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'grey', 'chatty', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'black', 'playful', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'brown', 'protective', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'white', 'sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 12, 'M', 'black', 'loner', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'white', 'shy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 14, 'M', 'brown', 'vicious', false);
